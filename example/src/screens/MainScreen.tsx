cbe47ea42e6a32d7be335cb6bfe1317979a31f66
keykat <koin12000@naver.com>
2026-09-11 13:41:58 +0900

    feat: 카테고리 추가

--- example/src/screens/MainScreen.tsx
+++ example/src/screens/MainScreen.tsx
@@ -2,6 +2,8 @@ import { StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { ToggleNavigation } from '../../../src/navigation/ToggleNavigation';
 import { HouseIcon } from 'phosphor-react-native/src/icons/House';
+import { MagnifyingGlassIcon } from 'phosphor-react-native/src/icons/MagnifyingGlass';
+import { UserIcon } from 'phosphor-react-native/src/icons/User';
 import { GearIcon } from 'phosphor-react-native/src/icons/Gear';
 
 export default function MainScreen() {
@@ -19,6 +21,26 @@ export default function MainScreen() {
             ),
             selectedIcon: <HouseIcon size={20} color="black" weight="fill" />,
             onPress: () => console.log('home'),
+          },
+          {
+            key: 'search',
+            label: 'Search',
+            unselectedIcon: (
+              <MagnifyingGlassIcon size={20} color="white" weight="regular" />
+            ),
+            selectedIcon: (
+              <MagnifyingGlassIcon size={20} color="black" weight="fill" />
+            ),
+            onPress: () => console.log('search'),
+          },
+          {
+            key: 'profile',
+            label: 'Profile',
+            unselectedIcon: (
+              <UserIcon size={20} color="white" weight="regular" />
+            ),
+            selectedIcon: <UserIcon size={20} color="black" weight="fill" />,
+            onPress: () => console.log('profile'),
           },
           {
             key: 'settings',
