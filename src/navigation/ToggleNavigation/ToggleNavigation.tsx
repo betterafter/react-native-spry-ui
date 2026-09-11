760e7b9390d8d858b46f2230ba191e66e7f33f37
keykat <koin12000@naver.com>
2026-09-11 13:40:55 +0900

    feat: 말줄임표 삭제

--- src/navigation/ToggleNavigation/ToggleNavigation.tsx
+++ src/navigation/ToggleNavigation/ToggleNavigation.tsx
@@ -198,7 +198,7 @@ function NavigationItem({
       <View style={styles.iconSlot}>{Icon}</View>
 
       <Animated.View style={[styles.labelContainer, labelAnimatedStyle]}>
-        <Text numberOfLines={1} style={styles.labelText}>
+        <Text numberOfLines={1} style={styles.labelText} ellipsizeMode="clip">
           {label}
         </Text>
       </Animated.View>
